3cf8191690b74793e38b1fdc967b00070c5c4b3a
Wesley Pribadi <16058246+wesley-pribadi@users.noreply.github.com>
2026-01-25 23:34:54 +0800

    A-TextUiTesting: Setup automated text UI testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# Get the directory where the script is located
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Change to that directory so relative paths work correctly
+cd "$SCRIPT_DIR"
+
+
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
+if [ ! -d "../out/production/ip" ]
 then
-    mkdir ../bin
+    # mkdir ../bin
+    echo "please mkdir ../out/production/ip"
+    exit 1
 fi
 
 # delete output from previous run
@@ -13,21 +22,22 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../out/production/ip ../src/main/java/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../out/production/ip Dyuque < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+#cp EXPECTED.TXT EXPECTED-UNIX.TXT
+#dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+#diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
