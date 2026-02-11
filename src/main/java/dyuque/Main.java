ea81eccd453ed5c2d5860d1214f44afcbc5cbff1
Wesley Pribadi <16058246+wesley-pribadi@users.noreply.github.com>
2026-02-12 00:36:36 +0800

    Level-10: Add JavaFX GUI

---
+++ src/main/java/dyuque/Main.java
@@ -0,0 +1,34 @@
+package dyuque;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Dyuque dyuque = Dyuque.initialiseDefaults();
+
+    public Main() throws DyuqueException {
+    }
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDyuque(dyuque);  // inject the Duke instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
